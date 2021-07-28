da0180ea23687c08d71394e23fe81627bef8d9d6
Brian Steuerman <33671768+bsteuerman832@users.noreply.github.com>
2021-07-28 11:17:07 -0400

    Add files via upload

--- EPL_data.R
+++ EPL_data.R
@@ -1,32 +1,34 @@
 # Load packages
 library(rio)
+library(readxl)
 
-rio_xlsx <- read_excel("R/EPL2021.xlsx")
+EPL2021 <- read_excel("R/EPL2021.xlsx")
 
 # Plots a club's position in the table versus both goals scored and conceded, connected by a line
-plot(rio_xlsx$GF, rio_xlsx$Pos, main = "EPL 20-21 team position vs goals scored",
+plot(EPL2021$GF, EPL2021$Pos, main = "EPL 20-21 team position vs goals scored",
      xlab = "Goals Scored",
      ylab = "Team position",
      pch = 19,
-     col = "blue")
+     col = c("lightblue", "red", "darkred", "blue", "dodgerblue", "brown4", 
+             "black", "red", "gold", "blue", "lightslateblue", "gray48", 
+             "yellow1", "blue", "orangered", "deepskyblue", "lightblue",
+             "black", "navy", "red"))
 
-summary(lm(rio_xlsx$Pos ~ rio_xlsx$GF)) #R-squared: 0.891,
-abline(lm(rio_xlsx$Pos ~ rio_xlsx$GF), col = "blue") # Slope = -0.3338
+summary(lm(EPL2021$Pos ~ EPL2021$GF)) #R-squared: 0.891,
+abline(lm(EPL2021$Pos ~ EPL2021$GF), col = "blue") # Slope = -0.3338
 
-plot(rio_xlsx$GA, rio_xlsx$Pos,
+plot(EPL2021$GA, EPL2021$Pos,
      main = "EPL 20-21 team position vs goals conceded",
      xlab = "Goals conceded",
      ylab = "Team position",
        col = "red", 
        pch = 19)
 
-summary(lm(rio_xlsx$Pos ~ rio_xlsx$GA)) #R-squared: 0.579
-abline(lm(rio_xlsx$Pos ~ rio_xlsx$GA), col = "red") # Slope = 0.4
-
-summary(rio_xlsx)
+summary(lm(EPL2021$Pos ~ EPL2021$GA)) #R-squared: 0.579
+abline(lm(EPL2021$Pos ~ EPL2021$GA), col = "red") # Slope = 0.4
 
 # You can also get the  correlation like this, square to get R^2:
-cor(rio_xlsx$GF, rio_xlsx$Pos) # Cor = -0.9439436
-cor(rio_xlsx$GA, rio_xlsx$Pos) # Cor = 0.7606566
+cor(EPL2021$GF, EPL2021$Pos) # Cor = -0.9439436
+cor(EPL2021$GA, EPL2021$Pos) # Cor = 0.7606566
 
 
