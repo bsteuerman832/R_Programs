bccbdd5bc3737045606a06c1f2315142d6f5bbdd
Brian Steuerman <33671768+bsteuerman832@users.noreply.github.com>
2021-07-28 18:07:20 -0400

    Add files via upload

--- EPL1920_data.R
+++ EPL1920_data.R
@@ -1,22 +1,39 @@
 # Load packages
-rio_xlsx2 <- read_excel("R/EPL1920.xlsx")
+library(calibrate)
+EPL1920 <- read_excel("R/EPL1920.xlsx")
+
+names = c("LIV","MCI","MU","CHE","LEI","TOT","WOL","ARS","SHU","BUR","SOU","EVE","NEW","CRY","BHA",
+          "WHU","AVL","BOU","WAT","NOR")
 
 # Plots a club's position in the table versus both goals scored and conceded, connected by a line
-plot(rio_xlsx2$GF, rio_xlsx2$Pos, main = "EPL 19-20 team position vs goals scored",
+plot(EPL1920$GF, EPL1920$Pos, main = "EPL 19-20 team position vs goals scored",
      xlab = "Goals Scored",
      ylab = "Team position",
      pch = 19,
-     col = "blue")
+     col = c("darkred", "lightblue", "red", "blue", "dodgerblue", "black", 
+             "yellow1", "red", "red", "lightblue", "orangered", "blue", 
+             "gray48", "blue", "orangered", "brown4", "lightslateblue",
+             "red", "yellow", "green3"))
 
-summary(lm(rio_xlsx2$Pos ~ rio_xlsx2$GF)) #R-squared: 0.725
-abline(lm(rio_xlsx2$Pos ~ rio_xlsx2$GF), col = "blue") 
+text(EPL1920$GF, EPL1920$Pos + 0.5, labels = names)
 
-plot(rio_xlsx2$GA, rio_xlsx$Pos,
+summary(lm(EPL1920$Pos ~ EPL1920$GF)) #R-squared: 0.725
+abline(lm(EPL1920$Pos ~ EPL1920$GF), col = "blue") 
+
+plot(EPL1920$GA, EPL1920$Pos,
      main = "EPL 19-20 team position vs goals conceded",
      xlab = "Goals conceded",
      ylab = "Team position",
-     col = "red", 
+     col = c("darkred", "lightblue", "red", "blue", "dodgerblue", "black", 
+             "yellow1", "red", "red", "lightblue", "orangered", "blue", 
+             "gray48", "blue", "orangered", "brown4", "lightslateblue",
+             "red", "yellow", "green3"), 
      pch = 19)
 
-summary(lm(rio_xlsx2$Pos ~ rio_xlsx2$GA)) #R-squared: 0.788
-abline(lm(rio_xlsx2$Pos ~ rio_xlsx2$GA), col = "red") 
+text(EPL1920$GA, EPL1920$Pos + 0.5, labels = names)
+
+summary(lm(EPL1920$Pos ~ EPL1920$GA)) #R-squared: 0.788
+abline(lm(EPL1920$Pos ~ EPL1920$GA), col = "red") 
+
+cor(EPL1920$GF, rio_xlsx$Pos) # Cor = -0.8513457
+cor(EPL1920$GA, rio_xlsx$Pos) # Cor = 0.8879449
--- EPL_data.R
+++ EPL_data.R
@@ -1,8 +1,12 @@
 # Load packages
 library(rio)
 library(readxl)
+library(calibrate)
 
 EPL2021 <- read_excel("R/EPL2021.xlsx")
+
+names = c("MCI","MUN","LFC","CHE","LEI","WHU","TOT","ARS","LEE","EVE","AVL","NEW","WOL","CRY","SOU",
+          "BHA","BUR","FUL","WBA","SHU")
 
 # Plots a club's position in the table versus both goals scored and conceded, connected by a line
 plot(EPL2021$GF, EPL2021$Pos, main = "EPL 20-21 team position vs goals scored",
@@ -11,8 +15,9 @@ plot(EPL2021$GF, EPL2021$Pos, main = "EPL 20-21 team position vs goals scored",
      pch = 19,
      col = c("lightblue", "red", "darkred", "blue", "dodgerblue", "brown4", 
              "black", "red", "gold", "blue", "lightslateblue", "gray48", 
-             "yellow1", "blue", "orangered", "deepskyblue", "lightblue",
+             "blue", "deepskyblue", "deepskyblue", "lightblue",
              "black", "navy", "red"))
+text(EPL2021$GF, EPL2021$Pos + 0.5, labels = names)
 
 summary(lm(EPL2021$Pos ~ EPL2021$GF)) #R-squared: 0.891,
 abline(lm(EPL2021$Pos ~ EPL2021$GF), col = "blue") # Slope = -0.3338
@@ -26,6 +31,7 @@ plot(EPL2021$GA, EPL2021$Pos,
                "yellow1", "blue", "orangered", "deepskyblue", "lightblue",
                "black", "navy", "red"), 
        pch = 19)
+text(EPL2021$GA, EPL2021$Pos + 0.5, labels = names)
 
 summary(lm(EPL2021$Pos ~ EPL2021$GA)) #R-squared: 0.579
 abline(lm(EPL2021$Pos ~ EPL2021$GA), col = "red") # Slope = 0.4
